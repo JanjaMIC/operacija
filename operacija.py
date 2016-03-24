91dfdebe7b132a46f88f53ecea897b46af3a1664
JanjaMIC <janjakrizman@icloud.com>
2016-03-24 16:58:48 +0100

    popravki

--- operacija.py
+++ operacija.py
@@ -2,6 +2,12 @@
 
 x = int(raw_input("Please insert number from 1 up to 100: "))
 
+while True:
+    if x < 1 or x > 100:
+        print "Stevilo ni pravilno"
+        x = int(raw_input("Please insert number from 1 up to 100: "))
+    else: break
+
 for x in range(1, x+1):
 
 
